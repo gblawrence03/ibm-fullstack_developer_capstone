de2648760bedec28b03c0a36ad0e3e89978e2a35
gblawrence03 <georgeblawrence@hotmail.co.uk>
2024-11-24 09:51:09 -0500

    PLEASE

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -50,9 +50,7 @@ def initiate():
       {"name": "Camry", "type": "Sedan",
        "year": 2023, "car_make": car_make_instances[4]},
       {"name": "Kluger", "type": "SUV",
-       "year": 2023, "car_make": car_make_instances[4]},
-        # Add more CarModel instances as needed
-      ]
+       "year": 2023, "car_make": car_make_instances[4]}]
     for data in car_model_data:
         CarModel.objects.create(name=data['name'], car_make=data['car_make'],
                                 car_type=data['type'], year=data['year'])
